16ebcf8d4c5d7f30a81b0d7bb71064e7c7e16ad9
tuborgstroem <tborgstroem@gmail.com>
2020-09-24 14:18:37 +0200

    Added dicecup class

--- src/main/java/game/Dice.java
+++ src/main/java/game/Dice.java
@@ -1,6 +1,6 @@
 
 
-package com.company;
+package game;
 
 
 import  java.util.Random;
---
+++ src/main/java/game/DiceCup.java
@@ -0,0 +1,35 @@
+package game;
+
+import java.util.ArrayList;
+
+public class DiceCup {
+    private int numberOfDice;
+    private ArrayList<Dice> diceIncup;
+
+    public DiceCup(int numOfDice, ArrayList<Integer> sidesOfDice) {
+        numberOfDice = numOfDice;
+        diceIncup = new ArrayList<Dice>();
+        for (int i = 0; i > numOfDice; i++) {
+            Dice d;
+            if (sidesOfDice.size() >= i) {
+                d = new Dice(sidesOfDice.get(i));
+                diceIncup.add(d);
+            } else {
+                d = new Dice();
+                diceIncup.add(d);
+            }
+        }
+    }
+    public DiceCup(int numOfDice){
+        numberOfDice = numOfDice;
+        diceIncup = new ArrayList<Dice>();
+        for (int i = 0; i>numOfDice; i++){
+            Dice d;
+            d = new Dice();
+            diceIncup.add(d);
+        }
+    }
+
+
+    }
+
