ea9b507f0bc8da8d42e76f392f8f958beead13a6
tuborgstroem <tborgstroem@gmail.com>
2020-09-24 12:10:50 +0200

    Added a class for dice

---
+++ src/com/company/Dice.java
@@ -0,0 +1,36 @@
+
+
+package com.company;
+
+
+import  java.util.Random;
+
+public class Dice {
+    private int value;
+    private Random rand;
+    private int sides;
+
+    public Dice(int numberOfSides){
+        rand = new Random();
+
+        sides = numberOfSides;
+    }
+    public Dice(){
+        rand = new Random();
+
+        sides = 6;
+    }
+
+    public int roll(){
+        value = rand.nextInt(sides) + 1;
+        return value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getSides(){
+        return sides;
+    }
+}
