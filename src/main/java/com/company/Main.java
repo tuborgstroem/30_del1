18a114108f0cec416fc6dd9bc4926ba6cfe3e0c1
tuborgstroem <tborgstroem@gmail.com>
2020-09-24 13:32:35 +0200

    Gui added

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import desktop_resources.GUI;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
         System.out.println("Hello World");
+        GUI.setDice(3, 4);
     }
 }
